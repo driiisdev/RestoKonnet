f684985406aee54fed8dbf0a455896bc77933881
Forster Asamany <asamanyforster@yahoo.com>
2024-02-28 18:16:28 -0500

    build: add logo

---
+++ client/src/components/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex gap-3">
+        <img class="w-5 sm:w-auto" src="../img/ramen_dining.png" alt="logo_icon">
+        <img class="w-32 sm:w-auto" src="../img/Restokonnet.png" alt="logo">
+    </div>
+</template>
